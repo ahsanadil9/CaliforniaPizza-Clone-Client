7bd22016b05e4e22f9c9dcac3571434ed09c1da8
Ahsan Adil <muhammadmahsanadil@gmail.com>
2023-11-02 08:10:30 +0500

    navbar logo on md and sm screen fix according to california

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,8 +5,8 @@ export default function Navbar() {
     <>
       {/* navbar */}
       <nav className="w-full bg-white border-b-2">
-        <div className="flex items-center px-4 py-2 h-14 lg:justify-center lg:h-16">
-          <div className="relative bg-white rounded-full w-14 aspect-square mb-[-3.5rem] lg:w-24 border-b-2 z-10">
+        <div className="flex items-center px-4 py-2 h-14 md:justify-center lg:justify-center lg:h-16">
+          <div className="relative bg-white lg:rounded-full w-14 aspect-square lg:mb-[-3.5rem] lg:w-24 lg:border-b-2 z-10">
             <Image
               src="/assets/california-logo.webp"
               alt="California Pizza Logo"
